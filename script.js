589ceeac414d652441d754c8ad106b8c35237c75
Daniel Dave M. Umali <134363236+DanielDave2004@users.noreply.github.com>
2026-03-06 14:42:02 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+const content = document.querySelector("#tableBody");
+const submit=document.querySelector("#add");
+
+//POST API
+submit.addEventListener('click',()=>{
+    let fullName=document.querySelector("#fName").value;
+    let course=document.querySelector("#cCourse").value;
+    let yearLevel=document.querySelector("#yLevel").value;
+    let email=document.querySelector("#eMail").value;
+    let formData={fullName,course,yearLevel,email};
+
+    fetch("http://localhost:1111/api/students",{
+        method:'POST',
+        body: JSON.stringify(formData),
+        headers:{
+            "Content-Type":"application/json",
+        },
+    }).catch((error)=>{
+        console.log(error);
+    })
+    alert("Student Added Successfully");
+    location.reload();
+});
+
+
+window.addEventListener('load', ()=>{
+    getStudents();
+})
+
+function getStudents(){
+    let html="";
+
+    fetch('http://localhost:1111/api/students',{mode:'cors'})
+    .then(response=>{
+        return response.json();
+    })
+    .then(data=>{
+        data.forEach(element => {
+            html += `
+            <tr>
+                <td>${element.fullName}</td>
+                <td>${element.course}</td>
+                <td>${element.yearLevel}</td>
+                <td>${element.email}</td>
+                <td>${element.dateEnrolled ? new Date(element.dateEnrolled).toLocaleDateString() : '—'}</td>
+            </tr>`;
+        });
+
+        content.innerHTML = html;
+    })
+    .catch(error=>{
+        console.log(error);
+    });
+}
